22b5c9d9873ff4072d261f892ed068a5aa0363af
khurshidbekweb <khurshidbeknuriddinov@gmail.com>
2025-05-27 17:01:02 +0500

    mange resole

--- next-i18next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next-i18next').UserConfig} */
-module.exports = {
-  i18n: {
-    defaultLocale: 'uz',
-    locales: ['uz', 'ru', 'en'],
-  },
-  defaultNS: 'common',
-  localePath: './public/locales',
-}
