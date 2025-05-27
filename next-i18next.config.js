e6ea6de3086f09bdd8e803590fa3f7bf7f6f9fcf
Nuriddinov Khurshidbek <131617038+khurshidbekweb@users.noreply.github.com>
2025-05-27 16:02:50 +0500

    Added next-i18next.config.js

---
+++ next-i18next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next-i18next').UserConfig} */
+module.exports = {
+  i18n: {
+    defaultLocale: 'uz',
+    locales: ['uz', 'ru', 'en'],
+  },
+  defaultNS: 'common',
+  localePath: './public/locales',
+}
