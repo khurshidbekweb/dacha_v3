1858cc51c211a936bbd8576fe2633e71abfd4f64
khurshidbekweb <khurshidbeknuriddinov@gmail.com>
2025-06-02 22:20:42 +0500

    form validation zod

--- constants/main-data.ts
+++ constants/main-data.ts
@@ -1,4 +1,5 @@
 import { User, MessageCircle } from 'lucide-react'
+import { z } from "zod";
 
 export const navLink = [
     {
@@ -20,3 +21,35 @@ export const navLink = [
         icon: MessageCircle
     },
 ]
+
+
+
+export const cottageSchema = z.object({
+    cottageName: z.string().min(1, "Cottage name is required"),
+    description: z.string().min(1, "Description is required"),
+    images: z.array(z.string()).optional(),
+    regionId: z.string().min(1, "Region is required"),
+    placeId: z.string().min(1, "Place is required"),
+    price: z.number().min(0, "Price cannot be negative"),
+    priceWeekend: z.number().min(0, "Weekend price cannot be negative"),
+    cottageType: z.array(z.string()).min(1, "At least one cottage type required"),
+    comforts: z.array(z.string()).optional(),
+    latitude: z.string(),
+    longitude: z.string(),
+    doubleBedCount: z.number().min(0),
+    entranceTime: z.string(),
+    exitTime: z.string(),
+    maxGuests: z.number().min(1, "At least 1 guest"),
+    numberOfRooms: z.number().min(1, "At least 1 room"),
+    singleBedCount: z.number().min(0),
+    familyOnly: z.boolean(),
+    noAlcohol: z.boolean(),
+    noLoudMusic: z.boolean(),
+    noParty: z.boolean(),
+    noPets: z.boolean(),
+    noSmoking: z.boolean(),
+    quiteHours: z.string(),
+});
+
+// Type’ni ham olamiz
+export type CottageFormValues = z.infer<typeof cottageSchema>;
--- section/add-new/main-info.tsx
+++ section/add-new/main-info.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const MainInfo = () => {
     return (
         <div>
-            Mian info
+            
         </div>
     );
 };
