3d3463d503fa6ac05c220ed3a5e6aafd6b10a6ba
khurshidbekweb <khurshidbeknuriddinov@gmail.com>
2025-06-12 21:17:37 +0500

    adds

--- section/profile/mobile/cottage.tsx
+++ section/profile/mobile/cottage.tsx
@@ -1,11 +1,18 @@
 import Navbar from '@/app/(root)/_components/navbar';
+import BreadCrumbs from '@/components/share/bredcrambs';
 import React from 'react';
+import { useTranslation } from 'react-i18next';
 
 const UserDacha = () => {
+    const { t } = useTranslation()
     return (
         <div className='w-full'>
             <Navbar />
-            dachalar bizda
+            <BreadCrumbs data={[{ slug: '/', title: t('home') }]} page={t('my_announcements')} />
+
+            <div className="flex flex-col space-y-3 mt-5">
+                <h3 className='text-2xl font-semibold'>{t('my_announcements')}</h3>
+            </div>
         </div>
     );
 };
