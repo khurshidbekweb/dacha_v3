aed46870a42416b175dae6d33f2acd09a6416a9b
khurshidbekweb <khurshidbeknuriddinov@gmail.com>
2025-06-13 21:52:19 +0500

    adds

--- app/(root)/services/[id]/page.tsx
+++ app/(root)/services/[id]/page.tsx
@@ -7,9 +7,11 @@ import BreadCrumbs from '@/components/share/bredcrambs';
 import { ALL_DATA } from '@/query/query-fn';
 import { TopBannerService } from '@/section/services/tarif-intro';
 import Navbar from '../../_components/navbar';
+import { useTranslation } from 'react-i18next';
 
 const Tariff = () => {
     const { id } = useParams()
+    const { t } = useTranslation()
     const { isLoading, data: tariffs, } = ALL_DATA.useTarifId(id);
     return (
         <>
@@ -17,7 +19,7 @@ const Tariff = () => {
             <BreadCrumbs data={[{ slug: '', title: 'Home' }, { slug: 'services', title: 'Services' }]} page="Tarif" />
             <div className='max-w-[1540px] mx-auto px-2 md:px-5 xl:px-14'>
                 <div className="flex flex-col items-start justify ">
-                    <h2 className='text-xl md:text-2xl font-createRound'>Tariff</h2>
+                    <h2 className='text-2xl md:text-3xl font-createRound mt-5 font-semibold'>{t('tariffs')}</h2>
                 </div>
                 {isLoading ? <SkeletonCard /> : <TopBannerService tarif={tariffs!} />}
             </div>
