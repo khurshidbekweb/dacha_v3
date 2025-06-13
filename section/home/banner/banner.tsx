68580a5b55eea32213e0b0184cb640fa35328088
khurshidbekweb <khurshidbeknuriddinov@gmail.com>
2025-06-13 21:58:18 +0500

    add banner skeleton

--- section/home/banner/banner.tsx
+++ section/home/banner/banner.tsx
@@ -5,13 +5,18 @@ import EmblaCarousel from "./corusel-main"
 
 import './css/embla.css'
 import { ALL_DATA } from "@/query/query-fn"
+import { SkeletonCardBanner } from "@/components/card/banner-skeleton"
 
 export function Banner() {
     const OPTIONS: EmblaOptionsType = { dragFree: true, loop: true }
-    const cottageTop = ALL_DATA.useCottageTop()?.data || [];
+    const { data: cottageTop, isLoading } = ALL_DATA.useCottageTop() || [];
     return (
         <div className="mx-auto">
-            <EmblaCarousel slides={cottageTop} options={OPTIONS} />
+            {isLoading || !cottageTop?.length ? <>
+                <SkeletonCardBanner />
+            </> :
+                <EmblaCarousel slides={cottageTop} options={OPTIONS} />
+            }
         </div>
     )
 }
