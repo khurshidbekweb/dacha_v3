093d009377b0eb135b0691eb6aa5b17e402b43ba
khurshidbekweb <khurshidbeknuriddinov@gmail.com>
2025-06-15 02:36:25 +0500

    adds

--- components/card/prmium-dacha.tsx
+++ components/card/prmium-dacha.tsx
@@ -4,6 +4,7 @@ import { useLikeStore } from "@/store/like-card";
 import { premiumCottage } from "@/types";
 import { Button } from "../ui/button";
 import { cn } from "@/lib/utils";
+import Link from "next/link";
 interface premiumCottageProps {
     dacha: premiumCottage
 }
@@ -15,52 +16,60 @@ export function PremiumDachaCard({ dacha }: premiumCottageProps) {
     const isLiked = likedCards.includes(dacha.cottage.id);
     const handleLikeClick = (id: string) => {
         toggleLike(id);
+
     };
     const view = dacha?.cottage?.events?.filter(event => event.eventType === 'view')
 
     return (
         <div className="w-[360px] md:w-[320px] overflow-hidden transition-shadow p-1 relative" key={dacha.id}>
-            <CardSwiper dacha={dacha.cottage} />
-
-            <div className="p-2">
-                <h3 className="font-medium text-lg">{dacha.cottage.name}</h3>
-                <div className="flex items-center text-sm text-gray-600 mt-1">
-                    <MapPin className="w-4 h-4 mr-1" />
-                    <span>{dacha.cottage.region.name.slice(0, 12)}., {dacha.cottage.place.name}</span>
-                </div>
-            </div>
-
-            <div className="p-2 pt-0">
-                <div className="flex gap-4 text-sm mb-3">
-                    <div className="flex items-center">
-                        <Users className="w-4 h-4 mr-1 text-gray-600" />
-                        <span>8 kishilik</span>
-                    </div>
-                    <div className="flex items-center">
-                        <DoorOpen className="w-4 h-4 mr-1 text-gray-600" />
-                        <span>4 xonalik</span>
-                    </div>
+            <Link href={`/view/${dacha.id}`} className="w-full flex flex-col" >
+                <div className="w-full pointer-events-none">
+                    <CardSwiper dacha={dacha.cottage} />
                 </div>
 
-                <div className="flex justify-between items-end">
-                    <div className="flex items-center text-sm text-gray-600">
-                        <Star className="w-4 h-4 mr-1 fill-yellow-400 text-yellow-400" />
-                        <span>0</span>
-                        <span className="mx-1">●</span>
-                        <span>0 Sharhlar</span>
+                <div className="card-content">
+                    <div className="p-2">
+                        <h3 className="font-medium text-lg">{dacha.cottage.name}</h3>
+                        <div className="flex items-center text-sm text-gray-600 mt-1">
+                            <MapPin className="w-4 h-4 mr-1" />
+                            <span>{dacha.cottage.region.name.slice(0, 12)}., {dacha.cottage.place.name}</span>
+                        </div>
                     </div>
-                    <div className="flex items-center gap-x-5">
-                        <span className="flex items-center gap-x-1 text-[14px]"><Eye size={16} /> {view.length}</span>
-                        <span className="flex items-center gap-x-1 text-[14px]"><Phone size={15} /> 23</span>
+
+                    <div className="p-2 pt-0">
+                        <div className="flex gap-4 text-sm mb-3">
+                            <div className="flex items-center">
+                                <Users className="w-4 h-4 mr-1 text-gray-600" />
+                                <span>8 kishilik</span>
+                            </div>
+                            <div className="flex items-center">
+                                <DoorOpen className="w-4 h-4 mr-1 text-gray-600" />
+                                <span>4 xonalik</span>
+                            </div>
+                        </div>
+
+                        <div className="flex justify-between items-end">
+                            <div className="flex items-center text-sm text-gray-600">
+                                <Star className="w-4 h-4 mr-1 fill-yellow-400 text-yellow-400" />
+                                <span>0</span>
+                                <span className="mx-1">●</span>
+                                <span>0 Sharhlar</span>
+                            </div>
+                            <div className="flex items-center gap-x-5">
+                                <span className="flex items-center gap-x-1 text-[14px]"><Eye size={16} /> {view.length}</span>
+                                <span className="flex items-center gap-x-1 text-[14px]"><Phone size={15} /> 29</span>
+                            </div>
+                        </div>
+
+                    </div>
+                    <div className="p-2 pt-0 flex justify-between items-center">
+                        <p className="text-lg font-semibold text-primary">{dacha.cottage.price.toLocaleString()} so`m</p>
+                        <Button className="p-2 rounded-lg bg-primary">Batafsil <MoveUpRight /></Button>
                     </div>
                 </div>
-            </div>
+            </Link>
 
-            <div className="p-2 pt-0 flex justify-between items-center">
-                <p className="text-lg font-semibold text-primary">{dacha.cottage.price.toLocaleString()} so`m</p>
-                <Button className="p-2 rounded-lg bg-primary">Batafsil <MoveUpRight /></Button>
-            </div>
-            <Button onClick={() => handleLikeClick(dacha.id)} variant='link' className='absolute  top-3 right-1'>
+            <Button onClick={() => handleLikeClick(dacha.id)} variant='link' className='absolute top-3 right-1'>
                 <span className='overflow-hidden bg-[#ffffff75] p-[6px] rounded-full' ><Heart className={cn('overflow-hidden text-black/50', isLiked && 'transition-colors fill-amber-400 text-yellow-500 ')} size={35} /></span>
             </Button>
             <div className="absolute top-3 left-3 flex gap-x-2 items-center rounded-full p-1 text-yellow-500 bg-[#ffffff76]">
