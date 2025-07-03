873cf24f374e24f70b00847b157d3209dd1c5f6d
khurshidbekweb <khurshidbeknuriddinov@gmail.com>
2025-07-04 00:09:46 +0500

    add like card

--- app/(root)/_components/mobile-nov.tsx
+++ app/(root)/_components/mobile-nov.tsx
@@ -1,4 +1,6 @@
 'use client'
+import { Badge } from '@/components/ui/badge';
+import { useLikeStore } from '@/store/like-card';
 import { Heart, Home, Plus, UserRound } from 'lucide-react';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
@@ -6,17 +8,21 @@ import React from 'react';
 
 const MobileNov = () => {
     const pathname = usePathname()
+    const likeNumber = useLikeStore(state => state.likedCards)
+    console.log(likeNumber);
+
     return (
         <div className='w-full fixed bottom-0 left-0 flex md:hidden justify-between items-center bg-secondary pt-3 pb-5  px-3 rounded-t-3xl'>
             <Link href={'/'} className={`flex flex-col items-center justify-center ${pathname === '/' ? 'text-[#008F8C]' : ''}`}>
                 <Home size={30} />
                 <p className={`text-[11px] ${pathname === '/' ? 'block' : 'hidden'}`}>Home</p>
             </Link>
-            <Link href={'/fovarite'} className={`flex flex-col items-center justify-center ${pathname === '/fovarite' ? 'text-[#008F8C]' : ''}`}>
+            <Link href={'/fovarite'} className={`flex flex-col items-center justify-center relative ${pathname === '/fovarite' ? 'text-[#008F8C]' : ''}`}>
                 <Heart size={30} />
                 <p className={`text-[11px] ${pathname === '/fovarite' ? 'block' : 'hidden'}`}>Fovarite</p>
+                {likeNumber?.length ? <Badge className='absolute -top-2 text-sm text-white -right-2 px-[6px] py-0 bg-red-400 '>{likeNumber?.length}</Badge> : ''}
             </Link>
-            <Link href={'/add-new'} className={`flex flex-col items-center justify-center ${pathname === '/add-new' ? 'text-[#008F8C]' : ''}`}>
+            <Link href={'/add-new'} className={`flex flex-col items-center justify-center  ${pathname === '/add-new' ? 'text-[#008F8C]' : ''}`}>
                 <Plus size={30} />
                 <p className={`text-[11px] ${pathname === '/add-new' ? 'block' : 'hidden'}`}>Add New</p>
             </Link>
